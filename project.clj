a461b1b48a8a0a8ecea3365ad954dd75a899ddb9
Michael Drogalis <mjd3089@rit.edu>
2015-04-24 12:34:09 -0700

    Bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.mdrogalis/rotating-seq "0.1.1"
+(defproject com.mdrogalis/rotating-seq "0.1.2"
   :description "A time expiring rotating sequence"
   :url "https://github.com/MichaelDrogalis/rotating-seq"
   :license {:name "Eclipse Public License"
